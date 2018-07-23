64e3af2ca6df6f00621f4e25734775252b20f14f
scalande@yahoo.com <scalande@yahoo.com>
2018-07-23 14:13:37 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -27,7 +27,7 @@ def retrieval
                  }
                  
                  
-                 hash["oil paintings"]
+                 shipping_manifest["oil paintings"]
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
